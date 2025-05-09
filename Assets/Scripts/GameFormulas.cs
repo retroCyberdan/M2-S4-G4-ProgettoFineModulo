2026aed42398861a267d96e9340e2d9a8eeae7a4
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-05-09 17:57:43 +0200

    Add several improvements + final scene

--- Assets/Scripts/GameFormulas.cs
+++ Assets/Scripts/GameFormulas.cs
@@ -1,18 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class GameFormulas : MonoBehaviour
+public static class GameFormulas
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool HasElementAdvantge (ELEMENT attackElement, Hero defender)
     {
-        
+        if (attackElement == defender.GetWeakness())
+        {
+            return true;
+        }
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool HasElementDisadvantage (ELEMENT attackElement, Hero defender)
     {
-        
+        if (attackElement == defender.GetResistance())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static float EvaluateElementalModifier(ELEMENT attackElement, Hero defender)
+    {
+        if (HasElementAdvantge(attackElement, defender) == true)
+        {
+            return 1.5f;
+        }
+        else if (HasElementDisadvantage(attackElement, defender) == true)
+        {
+            return 0.5f;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public static bool HasHit (Stats attacker, Stats defender)
+    {
+        int hitChance = attacker.aim - defender.eva;
+        int randomNumber = Random.Range(0, 100);
+        if (randomNumber > hitChance)
+        {
+            Debug.Log("MISS");
+            return false;
+        }
+        return true;
+    }
+
+    public static bool IsCrit (int critValue)
+    {
+        int randomNumber = Random.Range(0, 100);
+        if (randomNumber < critValue)
+        {
+            Debug.Log("CRIT");
+            return true;
+        }
+        return false;
+    }
+
+    public static int CalculateDamage(Hero attacker, Hero defender)
+    {
+        Stats attackerStatsSum = Stats.Sum(attacker.GetStats(), attacker.GetWeapon().GetStats());
+        Stats defenderStatsSum = Stats.Sum(defender.GetStats(), defender.GetWeapon().GetStats());
+        int defType;
+
+        if (attacker.GetWeapon().GetDmgType() == Weapon.DAMAGE_TYPE.PHYSICAL)
+        {
+            defType = defender.GetStats().def;
+        }
+        else
+        {
+            defType = defender.GetStats().res;
+        }
+
+        int baseDamage = attackerStatsSum.atk - defType;
+        float multiplier = EvaluateElementalModifier(attacker.GetWeapon().GetElem(), defender);
+        float finalDamage = baseDamage * multiplier;
+
+        if (IsCrit(Mathf.RoundToInt(finalDamage)) == true)
+        {
+            finalDamage *= 2;
+        }
+
+        if (finalDamage < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return Mathf.RoundToInt(finalDamage);
+        }
     }
 }
---
+++ Assets/Scripts/M1ProjectTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M1ProjectTest : MonoBehaviour
+{
+    public Hero a;
+    public Hero b;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if ()
+    }
+}
